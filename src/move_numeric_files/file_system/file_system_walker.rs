27e8e78e0bbf815923c805c63aea3b74427806a6
Mark English <thatmarkenglishguy@gmail.com>
2021-01-16 15:04:48 +0000

    tests - more of them and refactor.

--- src/move_numeric_files/file_system/file_system_walker.rs
+++ src/move_numeric_files/file_system/file_system_walker.rs
@@ -1,33 +1,25 @@
-use std::path::Path;
-
 struct FileWalker<'a> {
     root: &'a std::path::Path,
 }
 
-trait Walker {
-    fn visit(&mut self, path: &std::path::Path, directories: &[String], files: &[String]);
+trait Walker<I> {
+    fn visit(&mut self, root: &std::path::Path, directories: I, files: I);
 }
 
-impl<F> Walker for F
-where
-    F: Fn(&std::path::Path, &[String], &[String]),
-{
-    fn visit(&mut self, path: &Path, directories: &[String], files: &[String]) {
-        self(path, directories, files);
-    }
-}
-
-impl<'a> FileWalker<'a> {
+impl FileWalker<'_> {
     fn new(root: &std::path::Path) -> FileWalker {
         FileWalker { root }
     }
 
-    pub(crate) fn walk<W: Walker>(&self, walker: &mut W) -> std::io::Result<()> {
-        let mut directory_paths: Vec<std::path::PathBuf> = vec![];
+    pub(crate) fn walk<W>(&self, walker: &mut W) -> std::io::Result<()>
+    where
+        W: Walker<std::vec::IntoIter<String>>,
+    {
+        let mut directory_paths = vec![];
 
         {
-            let mut directories: Vec<String> = Vec::new();
-            let mut files: Vec<String> = Vec::new();
+            let mut directories = Vec::new();
+            let mut files = Vec::new();
 
             for entry in std::fs::read_dir(self.root)? {
                 let entry = entry?;
@@ -43,54 +35,97 @@ impl<'a> FileWalker<'a> {
                 }
             }
 
-            walker.visit(self.root, &directories, &files);
+            walker.visit(self.root, directories.into_iter(), files.into_iter());
         }
 
-        for directory_path in &directory_paths {
-            FileWalker::new(directory_path).walk(walker)?;
+        for directory_path in directory_paths.into_iter() {
+            let file_walker = FileWalker::new(&directory_path);
+            file_walker.walk(walker)?;
         }
 
         Ok(())
     }
 }
 
+impl<I, F> Walker<I> for F
+where
+    F: FnMut(&std::path::Path, I, I),
+{
+    fn visit(&mut self, root: &std::path::Path, directories: I, files: I) {
+        self(root, directories, files);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::move_numeric_files::file_system::file_system_walker::{FileWalker, Walker};
-    use crate::move_numeric_files::file_system::tests::file_adder::tests::FileAdder;
+    //use crate::move_numeric_files::file_system::tests::file_adder::tests::FileAdder;
+    use crate::move_numeric_files::file_system::tests::file_adder::tests::{
+        FileAdderOperations, TempFileAdder,
+    };
     use std::cell::RefCell;
-    use std::path::Path;
+
+    fn slice_to_vector<S, V>(slice: &[S], transmuter: impl Fn(S) -> V) -> Vec<V>
+        where S: Clone
+    {
+        slice
+            .iter()
+            .cloned()
+            .map(transmuter)
+            .collect::<Vec<_>>()
+    }
+
+    fn str_slice_to_string_vector(slice: &[&str]) -> Vec<String> {
+        slice_to_vector(slice, String::from)
+    }
 
     #[test]
     fn walk_test_files_with_closure() {
-        let test_files = vec!["first", "second"];
-        let file_adder = FileAdder::new().with_files_slice(&test_files);
+        let test_files = vec!["first", "second"]
+            .iter()
+            .cloned()
+            .map(String::from)
+            .collect::<Vec<_>>();
+        let file_adder = TempFileAdder::new().with_file_strings_slice(&test_files);
         let file_walker = FileWalker::new(file_adder.directory());
 
         // BOOOOOOO
-        let seen_directories: RefCell<Vec<String>> = RefCell::new(vec![]);
-        let seen_files: RefCell<Vec<String>> = RefCell::new(vec![]);
+        let seen_path = RefCell::new(vec![]);
+        let seen_directories = RefCell::new(vec![]);
+        let seen_files = RefCell::new(vec![]);
         let walk_executed = RefCell::new(false);
 
         file_walker
-            .walk(
-                &mut |parent: &std::path::Path, directories: &[String], files: &[String]| {
-                    (*seen_directories.borrow_mut()).extend_from_slice(directories);
-                    (*seen_files.borrow_mut()).extend_from_slice(files);
-                    (*walk_executed.borrow_mut()) = true;
-                    assert_eq!(directories.len(), 0);
-                    assert_eq!(parent, file_adder.directory());
-                },
-            )
+            .walk(&mut |path: &std::path::Path,
+                        directories: std::vec::IntoIter<_>,
+                        files: std::vec::IntoIter<_>| {
+                assert_eq!(
+                    directories.len(),
+                    0,
+                    "Expected 0 directories. Got [{}]",
+                    directories.clone().collect::<Vec<_>>().join(", ")
+                );
+                (*seen_path.borrow_mut()).push(path.to_path_buf());
+                (*seen_directories.borrow_mut()).extend(directories);
+                (*seen_files.borrow_mut()).extend(files);
+                (*walk_executed.borrow_mut()) = true;
+            })
             .unwrap();
 
         assert!(*walk_executed.borrow());
-        assert_eq!((*seen_directories.borrow()).len(), 0);
+        assert_eq!((*seen_path.borrow()), vec!(file_adder.directory()));
+        assert_eq!(
+            (*seen_directories.borrow()).len(),
+            0,
+            "Expected 0 seen directories. Got [{}]",
+            (*seen_directories.borrow()).join(", ")
+        );
         assert_eq!(*seen_files.borrow(), test_files);
     }
 
     /// Same as using closure but refactored into struct...
     struct TestWalker {
+        seen_paths: Vec<std::path::PathBuf>,
         seen_directories: Vec<String>,
         seen_files: Vec<String>,
         walk_executed: bool,
@@ -99,37 +134,107 @@ mod tests {
     impl TestWalker {
         fn new() -> TestWalker {
             TestWalker {
+                seen_paths: vec![],
                 seen_directories: vec![],
                 seen_files: vec![],
                 walk_executed: false,
             }
         }
 
-        fn verify(&self, test_directories: &[&str], test_files: &[&str]) {
+        fn verify<'p>(
+            &mut self,
+            test_paths: impl IntoIterator<Item = &'p std::path::Path>,
+            test_directories: impl Iterator<Item = String>,
+            test_files: impl Iterator<Item = String>,
+        ) {
+            let expected_directories = test_directories.collect::<Vec<_>>();
+            let expected_files = test_files.collect::<Vec<_>>();
+            let expected_paths = test_paths
+                .into_iter()
+                .map(|path| path.to_path_buf())
+                .collect::<Vec<_>>();
+
+            self.seen_files.sort();
+            self.seen_directories.sort();
+            self.seen_paths.sort();
             assert!(self.walk_executed);
-            assert_eq!(self.seen_directories, test_directories);
-            assert_eq!(self.seen_files, test_files);
+            assert_eq!(self.seen_paths, expected_paths);
+            assert_eq!(self.seen_directories, expected_directories);
+            assert_eq!(self.seen_files, expected_files);
         }
     }
 
-    impl Walker for TestWalker {
-        fn visit(&mut self, path: &Path, directories: &[String], files: &[String]) {
+    impl<I> Walker<I> for TestWalker
+    where
+        I: Iterator<Item = String>,
+    {
+        fn visit(&mut self, _path: &std::path::Path, directories: I, files: I) {
             self.walk_executed = true;
-            self.seen_directories.extend_from_slice(directories);
-            self.seen_files.extend_from_slice(files);
+            self.seen_paths.push(_path.to_path_buf());
+            self.seen_directories.extend(directories);
+            let relative_file_path = files
+                .map(|file_name| _path.join(file_name))
+                .map(|path| path.to_str().map(String::from).unwrap())
+            ;
+            self.seen_files.extend(relative_file_path);
         }
     }
 
     #[test]
     fn walk_test_files_with_trait_implementation() {
-        let expected_directories: Vec<&str> = vec![];
-        let test_files = vec!["first", "second"];
-        let file_adder = FileAdder::new().with_files_slice(&test_files);
+        let expected_directories: Vec<_> = vec![];
+        let test_files = str_slice_to_string_vector(&["first", "second"]);
+        let file_adder = TempFileAdder::new().with_file_strings_slice(&test_files);
+        let expected_files: Vec<_> = test_files
+            .iter()
+            .map(|subfilename| file_adder.relative_file_path(&std::path::PathBuf::from(subfilename)).to_str().map(String::from).unwrap())
+            .collect::<Vec<_>>()
+            ;
+        let expected_paths = vec![file_adder.directory()];
         let file_walker = FileWalker::new(file_adder.directory());
         let mut test_walker = TestWalker::new();
 
         file_walker.walk(&mut test_walker).unwrap();
 
-        test_walker.verify(&expected_directories, &test_files);
+        test_walker.verify(
+            expected_paths.into_iter(),
+            expected_directories.into_iter(),
+            expected_files.into_iter(),
+        );
+    }
+
+    #[test]
+    fn walk_test_sub_directory_files_with_trait_implementation() {
+        const SUBDIR_01: &str = "subdir01";
+
+        let expected_directories: Vec<_> = vec![String::from(SUBDIR_01)];
+        let test_files = str_slice_to_string_vector(&["first", "second"]);
+        let sub_test_files = str_slice_to_string_vector(&["first_sub", "second_sub"]);
+        let file_adder = TempFileAdder::new()
+            .with_file_strings_slice(&test_files)
+            .with_sub_directory(SUBDIR_01,
+                                |subdir01_adder|
+                                    subdir01_adder.with_file_strings_slice(&sub_test_files)
+            );
+        let subdir01_path = file_adder.relative_file_path(&std::path::PathBuf::from(SUBDIR_01));
+        let mut expected_files: Vec<_> = test_files
+            .iter()
+            .map(|subfilename| file_adder.relative_file_path(&std::path::PathBuf::from(subfilename)).to_str().map(String::from).unwrap())
+            .collect::<Vec<_>>()
+            ;
+        expected_files.extend(sub_test_files.iter().cloned().map(
+            |subfilename| subdir01_path.join(subfilename).to_str().map(String::from).unwrap()
+        ));
+        let expected_paths = vec![file_adder.directory(), subdir01_path.as_path()];
+        let file_walker = FileWalker::new(file_adder.directory());
+        let mut test_walker = TestWalker::new();
+
+        file_walker.walk(&mut test_walker).unwrap();
+
+        test_walker.verify(
+            expected_paths.into_iter(),
+            expected_directories.into_iter(),
+            expected_files.into_iter(),
+        );
     }
 }
--- src/move_numeric_files/file_system/tests/file_adder.rs
+++ src/move_numeric_files/file_system/tests/file_adder.rs
@@ -2,29 +2,31 @@
 pub(crate) mod tests {
     use tempdir::TempDir;
 
-    pub(crate) struct FileAdder {
-        directory: tempdir::TempDir,
+    pub(crate) struct SubdirectoryFileAdder {
+        sub_directory: std::path::PathBuf,
     }
 
-    impl FileAdder {
-        const TEMPDIR_PREFIX: &'static str = "test_filesystem_walker";
+    impl SubdirectoryFileAdder {
+        pub(crate) fn new(sub_directory: std::path::PathBuf) -> Self {
+            std::fs::create_dir(&sub_directory).unwrap();
+            Self { sub_directory }
+        }
+    }
+
+    pub(crate) trait FileAdderOperations {
+        fn directory(&self) -> &std::path::Path;
 
         fn relative_file_path(&self, file_sub_path: &std::path::PathBuf) -> std::path::PathBuf {
-            self.directory.path().join(file_sub_path)
+            self.directory().join(file_sub_path)
         }
 
-        pub(crate) fn new() -> FileAdder {
-            FileAdder {
-                directory: TempDir::new(Self::TEMPDIR_PREFIX)
-                    .expect(format!("Unable to create {}", Self::TEMPDIR_PREFIX).as_str()),
-            }
-        }
-
-        /// Add files to temp directory
         fn with_file_paths<'a>(
             self,
             file_paths_iterator: impl Iterator<Item = std::path::PathBuf>,
-        ) -> FileAdder {
+        ) -> Self
+        where
+            Self: Sized
+        {
             for file_sub_path in file_paths_iterator {
                 let file_path = self.relative_file_path(&file_sub_path);
                 std::fs::File::create(file_path).unwrap();
@@ -32,21 +34,69 @@ pub(crate) mod tests {
             self
         }
 
-        pub(crate) fn with_files<'a>(
-            self,
-            file_paths_iterator: impl Iterator<Item = &'a str>,
-        ) -> Self {
+        fn with_files<'a>(self, file_paths_iterator: impl Iterator<Item = &'a str>) -> Self
+        where
+            Self: Sized
+        {
             let file_path_bufs =
                 file_paths_iterator.map(|p| -> std::path::PathBuf { std::path::PathBuf::from(p) });
             self.with_file_paths(file_path_bufs)
         }
 
-        pub(crate) fn with_files_slice(self, file_paths_slice: &[&str]) -> Self {
+        fn with_files_slice(self, file_paths_slice: &[&str]) -> Self
+        where
+            Self: Sized
+        {
             self.with_files(file_paths_slice.iter().cloned())
         }
 
-        pub(crate) fn directory(&self) -> &std::path::Path {
+        fn with_file_strings_slice(self, file_paths_slice: &[String]) -> Self
+        where
+            Self: Sized
+        {
+            self.with_files(file_paths_slice.iter().map(|string| string.as_str()))
+        }
+
+        fn with_sub_directory<A>(
+            self,
+            sub_directory: &str,
+            sub_directory_file_adder_fn: impl Fn(SubdirectoryFileAdder) -> A,
+        ) -> Self
+        where
+            Self: Sized
+        {
+            let sub_directory_path = self.relative_file_path(&std::path::PathBuf::from(sub_directory));
+            let sub_directory_file_adder = SubdirectoryFileAdder::new(sub_directory_path);
+            let _sub_directory_adder = sub_directory_file_adder_fn(sub_directory_file_adder);
+
+            self
+        }
+    }
+
+    pub(crate) struct TempFileAdder {
+        directory: tempdir::TempDir,
+    }
+
+    impl TempFileAdder {
+        const TEMPDIR_PREFIX: &'static str = "test_filesystem_walker";
+
+        pub(crate) fn new() -> TempFileAdder {
+            TempFileAdder {
+                directory: TempDir::new(Self::TEMPDIR_PREFIX)
+                    .expect(format!("Unable to create {}", Self::TEMPDIR_PREFIX).as_str()),
+            }
+        }
+    }
+
+    impl FileAdderOperations for TempFileAdder {
+        fn directory(&self) -> &std::path::Path {
             self.directory.path()
+        }
+    }
+
+    impl FileAdderOperations for SubdirectoryFileAdder {
+        fn directory(&self) -> &std::path::Path {
+            self.sub_directory.as_path()
         }
     }
 
@@ -59,14 +109,80 @@ pub(crate) mod tests {
         };
     }
 
+    macro_rules! assert_file_does_not_exist {
+        ($file_path:expr) => {
+            assert!(
+                ! $file_path.exists(),
+                format!("file path does exist: {}", $file_path.to_str().unwrap())
+            );
+        };
+    }
+
+    #[test]
+    fn test_deletes_temp_file_adder_directory() {
+        let file_adder_directory: std::path::PathBuf;
+        {
+            let file_adder = TempFileAdder::new()
+                .with_files_slice(&vec!["file_1", "file_2"])
+                .with_sub_directory("sub01", |sub_adder| sub_adder.with_files_slice(&["sub01.txt"]))
+                ;
+            file_adder_directory = file_adder.directory().to_path_buf();
+            assert_file_exists!(file_adder_directory);
+        }
+
+        assert_file_does_not_exist!(file_adder_directory);
+    }
+
+
     #[test]
     fn test_create_test_files() {
         let test_files = vec!["file_1", "file_2"];
 
-        let file_adder = FileAdder::new().with_files_slice(&test_files);
+        let file_adder = TempFileAdder::new().with_files_slice(&test_files);
 
         for test_file in test_files {
             let file_path = file_adder.relative_file_path(&std::path::PathBuf::from(test_file));
+            assert_file_exists!(file_path);
+        }
+    }
+
+    #[test]
+    fn test_create_test_files_in_a_subdirectory_using_closure_without_environment() {
+        const SUBDIR_01: &str = "subdir01";
+        const SUBDIR01_FILE01: &str = "file01.txt";
+        const SUBDIR01_FILE02: &str = "file02.txt";
+
+        let file_adder = TempFileAdder::new().with_sub_directory(SUBDIR_01, |subdir|
+            subdir.with_files_slice(&vec![SUBDIR01_FILE01, SUBDIR01_FILE02])
+        );
+
+        for file_name in [SUBDIR01_FILE01, SUBDIR01_FILE02].iter() {
+            let file_path = file_adder.relative_file_path(
+                &[SUBDIR_01, file_name]
+                    .iter()
+                    .collect::<std::path::PathBuf>(),
+            );
+            assert_file_exists!(file_path);
+        }
+    }
+
+    #[test]
+    fn test_create_test_files_in_a_subdirectory_using_closure_with_environment() {
+        const SUBDIR_01: &str = "subdir01";
+        const SUBDIR01_FILE01: &str = "file01.txt";
+        const SUBDIR01_FILE02: &str = "file02.txt";
+        let subdirectory_files = vec![SUBDIR01_FILE01, SUBDIR01_FILE02];
+
+        let file_adder = TempFileAdder::new().with_sub_directory(SUBDIR_01, |subdir|
+            subdir.with_files_slice(&subdirectory_files)
+        );
+
+        for file_name in [SUBDIR01_FILE01, SUBDIR01_FILE02].iter() {
+            let file_path = file_adder.relative_file_path(
+                &[SUBDIR_01, file_name]
+                    .iter()
+                    .collect::<std::path::PathBuf>(),
+            );
             assert_file_exists!(file_path);
         }
     }
